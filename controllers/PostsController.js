c9706bdb16cf31c35001bb8fa5087a5de0a32018
clintonngotta <clintonngotta@gmail.com>
2022-03-17 08:48:06 +0300

    posts

--- controllers/PostsController.js
+++ controllers/PostsController.js
@@ -5,11 +5,25 @@ const bcrypt = require("bcrypt");
 const postsModel = require("../models/Posts");
 
 const getAllPosts = (req, res) => {
-  res.send({ posts: [] });
+  postsModel
+    .find()
+    .then(function (posts) {
+      if (posts) {
+        res.status(200).json(posts);
+      } else {
+        res.status(404).json({ message: "error occured" });
+      }
+    })
+    .catch((error) => {
+      if (error) {
+        throw error;
+      }
+    });
 };
 
-const getSinglePost = (req, res) => {
-  res.send({ posts: [] });
+const getSinglePost = async (req, res, next) => {
+  let post = await getPost(req, res, next);
+  res.send(post);
 };
 
 const createNewPost = async (req, res) => {
@@ -34,14 +48,57 @@ const createNewPost = async (req, res) => {
   }
 };
 
-const updatePost = (req, res) => {
-  res.send("updatePost");
+const updatePost = async (req, res) => {
+  postsModel
+    .findByIdAndUpdate({ _id: req.params.id }, req.body)
+    .then(function () {
+      postsModel.findOne({ _id: req.params.id }).then(function (post) {
+        res.send(post);
+      });
+    })
+    .catch((error) => {
+      if (error) {
+        throw error;
+        res.json(err);
+      }
+    });
 };
 
 const deletePost = (req, res) => {
-  res.send("deletePost");
+  postsModel
+    .findByIdAndRemove({ _id: req.params.id })
+    .then(function (err, post) {
+      if (post) {
+        res.status(200).send({
+          message: "post deleted",
+          post: post,
+        });
+      } else {
+        res.status(404).send("post of id " + req.params.id + " not found");
+      }
+    })
+    .catch((error) => {
+      if (error) {
+        throw error;
+      }
+    });
 };
 
+async function getPost(req, res, next) {
+  let post;
+  try {
+    post = await postsModel.findById(req.params.id);
+    console.log(post);
+    if (post == null) {
+      return res.status(404).json({ message: "Cannot find post" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.post = post;
+  next();
+}
 module.exports = {
   getAllPosts,
   getSinglePost,

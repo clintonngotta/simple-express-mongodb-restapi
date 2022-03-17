de7676bd9d1de9488c22f16133cf923dde070f3f
clintonngotta <clintonngotta@gmail.com>
2022-03-17 07:32:57 +0300

    auth

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,22 @@
+// import models
+const usersModel = require("../models/Users");
+
 const getAllUsers = (req, res) => {
+  usersModel
+    .find()
+    .then(function (users) {
+      if (users) {
+        res.status(200).json(users);
+      } else {
+        res.status(404).json({ message: "error occured" });
+      }
+    })
+    .catch((error) => {
+      if (error) {
+        throw error;
+      }
+    });
+
   res.send({ users: [] });
 };
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 // import controllers
 const AuthController = require("../controllers/AuthController");
 
 // auth routes
-router.post("/login", AuthController.loginUser);
-router.post("/register", AuthController.registerUser);
+router.post(
+  "/login",
+  body("username").exists().withMessage("Username is Requiered"),
+  body("password").exists().withMessage("Password is Requiered"),
+  AuthController.loginUser
+);
+router.post(
+  "/register",
+  body("username").exists().withMessage("Username is Requiered"),
+  body("password").exists().withMessage("Password is Requiered"),
+  AuthController.registerUser
+);
 
 module.exports = router;

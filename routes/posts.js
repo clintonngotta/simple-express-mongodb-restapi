c8a0dcdc1a1035af58552312b919d013bed96520
clintonngotta <clintonngotta@gmail.com>
2022-03-17 07:48:07 +0300

    add post

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
 
 // import controllers
 const PostsController = require("../controllers/PostsController");
@@ -7,7 +8,13 @@ const PostsController = require("../controllers/PostsController");
 // posts routes
 router.get("/", PostsController.getAllPosts);
 router.get("/:id", PostsController.getSinglePost);
-router.post("/create", PostsController.createNewPost);
+router.post(
+  "/create",
+  body("description").exists().withMessage("description is Requiered"),
+  body("title").exists().withMessage("title is Requiered"),
+  body("author").exists().withMessage("author is Requiered"),
+  PostsController.createNewPost
+);
 router.patch("/update/:id", PostsController.updatePost);
 router.delete("/delete/:id", PostsController.deletePost);
 
